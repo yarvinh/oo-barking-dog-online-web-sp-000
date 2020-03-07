a4024001afff8dd0d88936776f7f427439b8b7d3
binding-archive-7591 <yarhumher@hotmail.com>
2020-03-07 23:28:39 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Dog
+  def name=(dog_name)
+    @dog_name = dog_name
+  end
+  def name
+    @dog_name
+  end
+  def bark
+    puts "woof!"
+  end
+end
+fido = Dog.new
+fido.name=("Fido")
